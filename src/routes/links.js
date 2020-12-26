56707f9f155c5d6f246e2d987f43b2cea9f31989
Nahuel Castro <nahukas@gmail.com>
2020-12-26 17:23:44 -0300

    Finish query from myslq

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,18 +4,23 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/add', (req, res) => {
-    res.render('links/add');
+  res.render('links/add');
 });
 
 router.post('/add', async (req, res) => {
-    const { title, url, description } = req.body;
-    const newLink = {
-        title,
-        url,
-        description
-    };
-    await pool.query('INSERT INTO links SET ?', [newLink]);
-    res.send('Received');
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  await pool.query('INSERT INTO links SET ?', [newLink]);
+  res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+  const links = await pool.query('SELECT * FROM links');
+  res.render('links/list', { links });
 });
 
 module.exports = router;
